45590b37c7759fb40693ccb06f14c3ad1e2484c4
rhuanpablo13 <rhuanpablo13@hotmail.com>
2020-10-04 02:54:55 -0300

    order, instant

--- src/main/java/com/rhuan/demo/entities/User.java
+++ src/main/java/com/rhuan/demo/entities/User.java
@@ -1,13 +1,18 @@
 package com.rhuan.demo.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "TB_USER")
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -19,6 +24,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	// mapeamento opcional
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		// TODO Auto-generated constructor stub
@@ -34,7 +43,17 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
-
+	
+	public User(Long id, String name, String email, String phone, String password, List<Order> orders) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+		this.orders = orders;
+	}
+	
 	
 	@Override
 	public String toString() {
@@ -119,5 +138,9 @@ public class User implements Serializable {
 		this.password = password;
 	}
 	
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 	
 }
