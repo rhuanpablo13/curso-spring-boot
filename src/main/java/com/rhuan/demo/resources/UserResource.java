1e52d5227ff1ffd7c16a2323745184fc2f57a306
rhuanpablo13 <rhuanpablo13@hotmail.com>
2020-10-04 02:08:24 -0300

    camada de servico, registro de componentes e endpoint findById

--- src/main/java/com/rhuan/demo/resources/UserResource.java
+++ src/main/java/com/rhuan/demo/resources/UserResource.java
@@ -1,21 +1,32 @@
 package com.rhuan.demo.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rhuan.demo.entities.User;
+import com.rhuan.demo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Rhuan", "rhuan@email.com", "4586456456", "123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {		
+		return ResponseEntity.ok().body(userService.findAll());
 	}
 	
-	public void teste() {}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		return ResponseEntity.ok().body(userService.findById(id));
+	}
+	
 }
